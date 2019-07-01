84070e6f88f0ea0a6babcf934bab682f5714f556
Thiago <ThiagoDAraujoS@Gmail.com>
2019-06-30 20:43:46 -0700

    Added Menu scene
    Added the intro menu scene for the game

---
+++ Assets/Scripts/Scene Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    private static SceneManager singleton;
+
+    public void GoToNewLevel(){
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
