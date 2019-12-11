f0aafc73338699bb23260bfb412c65f9c051d756
Thiago <ThiagoDAraujoS@Gmail.com>
2019-12-10 22:08:38 -0800

    Added Hexagon Test elements

---
+++ Assets/Scripts/HexagonMatExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonMatExample : MonoBehaviour
+{
+    Hex.Board<int> matrix = new Hex.Board<int>(5, 5, 1f);
+    // Start is called before the first frame update
+    void Start() {
+        matrix.Foreach((Hex.Vector index, ref int content) => {
+            GameObject o = new GameObject();
+            o.transform.position = matrix.Position(index);
+            o.name = index.ToString();
+            o.transform.parent = transform;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
